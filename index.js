db377d4f94445ee3cdb95ce01eb93e1cbb1c0a82
wayneathomas <31037340+wayneathomas@users.noreply.github.com>
2021-11-06 14:07:11 -0400

    Input Validation
    Input validation for IP address entered

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('test')
+let ipRegExp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g;
+
+document.querySelector('form button').addEventListener('click', test);
+
+function test(e) {
+    e.preventDefault();
+    if ((document.querySelector('form input').value).match(ipRegExp))
+        console.log('correct')
+    else
+        console.log('fail');
+} 
